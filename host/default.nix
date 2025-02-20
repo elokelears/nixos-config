8d82118edaaa90f55791957a06f123ff528e916a
elokelears <elokelears@gmail.com>
2025-02-21 04:07:18 +0800

    basic system config

---
+++ host/default.nix
@@ -0,0 +1,78 @@
+{ pkgs, hostname, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # The basic system configuration.
+
+  # BootLoader
+  bboot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking
+  networking = {
+    hostName = hostname;
+    networkManager.enable = true;
+  };
+
+  # Set up the time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # Configure the system locale.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system and configure the X11 keyboard layout.
+  services.xserver.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account.
+  users.users.elokelears = {
+    isNormalUser = true;
+    description = "elokelears";
+    home = "/home/elokelears";
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable the feature flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
