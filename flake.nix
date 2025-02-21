a8b7d7ae019122699f2a43559782eb5ab9455f67
elokelears <elokelears@gmail.com>
2025-02-21 08:59:53 +0800

    flake-update

--- flake.nix
+++ flake.nix
@@ -3,30 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #home-manager = {
-    #  url = "github:nix-community/home-manager";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
   let
     system = "x86_64-linux";
-    #username = "elokelears";
-    #stateVersion = "24.11";
+    username = "elokelears";
+    stateVersion = "24.11";
+    pkgs = nixpkgs.legacyPackages.${system};
+    
+    home = import ./home/home.nix {
+      inherit pkgs username stateVersion;
+    };
 
   in {
     nixosConfigurations = {
       NixOS = nixpkgs.lib.nixosSystem {
         inherit system;
-        #specialArgs = { inherit hostname username stateVersion ; };
+        specialArgs = { inherit pkgs username stateVersion ; };
         modules = [
           ./host
-          #home-manager.nixosModules.home-manager
-          #{
-           # home-manager.useGlobalPkgs = true;
-           # home-manager.useUserPackages = true;
-          #}
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.user.${username} = home;
+          }
         ];
       };
     };
