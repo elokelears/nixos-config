21b57f65780c201873d2c16f685170f6714cb865
elokelears <elokelears@gmail.com>
2025-02-21 19:20:35 +0800

    update inputmethod

---
+++ host/fcitx5-rime/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  i18n.inputMethod = {
+      type = "fcitx5";
+      enable = true;
+      fcitx5.addons = with pkgs; [
+          fcitx5-gtk
+          kdePackages.fcitx5-qt
+          fcitx5-rime
+      ];
+  };
+}
